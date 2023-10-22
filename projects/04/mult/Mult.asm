b22fcab5c8518cbc5ec9a0f5458b18402b7196f6
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-10-22 05:51:14 +0530

    Implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,30 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+    @R0
+    D=M; // D now holds one of the inputs
+    @i
+    M=D-1; // initialize a counter with the input; itni baar loop krenge and R2 ka content sum mei add krenge
+    @R2
+    M=0;
+(LOOP)
+    
+    @i
+    D=M;   // idhar i ko 
+    M=D-1; // reduce krkr check kr rhe
+
+    @END
+    D;JLT
+
+    @R1     // Is part mei 
+    D=M;    //  add kr rhe hai R2
+    @R2     // mei R1 ki values
+    M=M+D;  // 
+    
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP // Infinite loop !
